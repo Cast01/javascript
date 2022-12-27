039c96d7f6e4c3467b560e8264c1cfe455c0feb0
Cast01 <gabriel_floriano_castilho@outlook.com>
2022-12-27 15:05:07 -0300

    UPDATE(operadores-logicos.js)

--- 005-operadores-logicos/operadores-logicos.js
+++ 005-operadores-logicos/operadores-logicos.js
@@ -4,7 +4,8 @@
  * Exemplos de expressões que podem ser convertidas para falso são aquelas que são avaliados como null, 0, string vazia ("") ou undefined.
  */
 
-// Operador lógico '&&' retorna o ultimo valor da comparação se todos forem true/existir.
+// Operador lógico '&&' retorna o ultimo valor da comparação se todos antes dele forem true/existir e o mesmo for true/existir também.
+// Operador lógico '&&' retorna o primeiro valor false que encontrar.
 console.log("Operador '&&' só retorna true se ambas as comparaçoes forem true: " + (true && true));
 console.log("Operador '&&' só retorna true se ambas as comparaçoes forem true: " + (true && false));
 console.log("Operador '&&' só retorna true se ambas as comparaçoes forem true: " + (0 && true));
@@ -12,11 +13,18 @@ console.log("Operador '&&' só retorna true se ambas as comparaçoes forem true:
 
 console.log('\n');
 
-// Operador lógico '||' retorna o primeiro valor da comparação se um ou outro for true/existir.
-console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + (false || false));
-console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + (true || false));
+// Operador lógico '||' retorna o primeiro valor que for true/existir.
+// Operador lógico '||' retorna o ultimo valor da comparação se todos antes dele forem false/inexistir e o mesmo for false/inexistir também.
+console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + (false || null));
+console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + ("opa" || false));
 console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + (0 || true));
 console.log("Operador '||' só retorna false se ambas as comparaçoes forem false: " + (4 || 44 || 3));
+
+console.log('\n');
+
+// Operador lógico hibrido.
+console.log('' && 'asd' || 'eba'); // eba
+console.log('' && 'teste' || 0 && true); // 0
 
 console.log('\n');
 
